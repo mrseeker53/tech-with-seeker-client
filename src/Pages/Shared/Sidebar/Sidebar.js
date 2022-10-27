3b6d3451e206ed97321707d11fdb56ca2fabaf40
mrseeker53 <mi.nayeem30@gmail.com>
2022-10-27 07:15:26 +0600

    load data for  sidebar

--- src/Pages/Shared/Sidebar/Sidebar.js
+++ src/Pages/Shared/Sidebar/Sidebar.js
@@ -3,7 +3,15 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 
 const Sidebar = () => {
-    
+    // Declare a state to set data from server
+    const [courses, setCourses] = useState([]);
+
+    // // Declare useEffect to load data for non route element
+    useEffect(() => {
+        fetch('')
+            .then(res => res.json())
+            .then(data => setCourses(data));
+    }, [])
 
     return (
         <div>
