bc5d1169d7ad30a17ced8555e802971458e64132
mrseeker53 <mi.nayeem30@gmail.com>
2022-10-27 07:13:56 +0600

    added sidebar

---
+++ src/Pages/Shared/Sidebar/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+const Sidebar = () => {
+    
+
+    return (
+        <div>
+            <h5>Sidebar</h5>
+        </div>
+    );
+};
+
+export default Sidebar;
